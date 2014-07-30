5255074e40b4651e1488aa0508f7461dd96ef2af
Robert Mays Jr <rmays@castlecomm.com>
2014-07-30 18:22:11 -0400

    Added Menu Option to Reverse Animation, Added Menu option + shortcut keys to step forward and backwards through animation

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -75,6 +75,7 @@ protected:
 private slots:
 	void updatedFps();
 	void updateTVal();
+	void updateTVal(bool reverse);
         void updateMdiMode(bool mdi);
         void updateUndockMode(bool undockMode);
 	void setFileName(const QString &filename);
@@ -194,6 +195,8 @@ public slots:
 	void viewModeShowAxes();
 	void viewModeShowCrosshairs();
 	void viewModeAnimate();
+	void viewAnimateBack();
+	void viewAnimateFwd();
 	void viewAngleTop();
 	void viewAngleBottom();
 	void viewAngleLeft();
